8991a1d084eb019238c42aaf0853ef3a9ee13477
Alessandro Chumpitaz <par_214@hotmail.com>
2023-11-06 10:42:39 -0500

    :construction: wip on principal form

--- app/calculate/page.tsx
+++ app/calculate/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+import AppLayout from "@/components/shared/layouts/app-layout";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { PrefixedInput } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import React from "react";
+
 export default function CalculatePage() {
   return (
-    <div>
-      <h1>Calculate</h1>
-    </div>
+    <AppLayout>
+      <section className="p-10 w-full h-full flex flex-col items-start">
+        <h1 className="text-4xl mb-2">Nuevo cálculo</h1>
+        <section className="w-full flex justify-between">
+          <Card>
+            <CardHeader>
+              <CardTitle>Datos de entrada</CardTitle>
+              <CardDescription>
+                Datos iniciales para empezar el cálculo del plan de pagos
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="loanAmount">Monto total</Label>
+                    <PrefixedInput
+                      id="loanAmount"
+                      type="number"
+                      placeholder="Monto total del préstamo"
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="loanAmount">Monto total</Label>
+                    <Select>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Seleccionar Moneda" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="apple">
+                            Dólar estadounidense
+                          </SelectItem>
+                          <SelectItem value="banana">Sol peruano</SelectItem>
+                          <SelectItem value="blueberry">
+                            Peso mexicano
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+          <Card>Section 2</Card>
+        </section>
+      </section>
+    </AppLayout>
   );
 }
